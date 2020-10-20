66e88e63bf2d69f7ac417ac15b47f8c0622a1bbe
99002474 <safura.zohareen@ltts.com>
2020-10-20 15:08:41 +0530

    Commit

---
+++ gitdemo/firstnetcoreapp/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+
+namespace FirstNetCoreApp
+{
+    public delegate int MathOpDelegate(int M,int N);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(DateTime.Now.ToString());
+            Console.WriteLine("Result of Add:\t");
+        }
+    }
+}
